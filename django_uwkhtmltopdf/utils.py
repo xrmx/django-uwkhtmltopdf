93ce547122b734eaed0ae2f3846ffed48d926278
Daniele Masera <daniele.masera@libero.it>
2013-07-24 17:42:49 +0200

    Pass options to generate_pdf as a dictionary
    Instead of polluting the function with parameters use a single
    dict.
    
    WARNING: This breaks Backward Compatiblity but we are so early in
    development and so not popular that this should not be a problem
    in practice.

--- django_uwkhtmltopdf/utils.py
+++ django_uwkhtmltopdf/utils.py
@@ -23,7 +23,7 @@ def render_to_tmp_file(template_name, context):
     tmp.close()
     return name
 
-def generate_pdf(template_name, file_object=None, context=None, header=None, footer=None):
+def generate_pdf(template_name, file_object=None, context=None, options=None):
     """
     Uses wkhtmltopdf to render a PDF to the passed file_object, from the
     given template name. Optionally takes a template name for an header and for a footer
@@ -39,20 +39,43 @@ def generate_pdf(template_name, file_object=None, context=None, header=None, foo
     tmpl = get_template(template_name)
     html = smart_str(tmpl.render(Context(context)))
 
+    if options is None:
+        options = {}
+
     opts = []
-    if header:
-        header_template = render_to_tmp_file(header, context)
-        opts.extend(['--header-html', header_template])
-    if footer:
-        footer_template = render_to_tmp_file(footer, context)
-        opts.extend(['--footer-html', footer_template])
+    if options:
+        if 'header' in options:
+            header_template = render_to_tmp_file(options['header'], context)
+            opts.extend(['--header-html', header_template])
+
+        if 'footer' in options:
+            footer_template = render_to_tmp_file(options['footer'], context)
+            opts.extend(['--footer-html', footer_template])
+
+        if 'margin-top' in options:
+            opts.extend(['--margin-top', options['margin-top']])
+
+        if 'margin-bottom' in options:
+            opts.extend(['--margin-bottom', options['margin-bottom']])
+
+        if 'margin-left' in options:
+            opts.extend(['--margin-left', options['margin-left']])
+
+        if 'margin-right' in options:
+            opts.extend(['--margin-right', options['margin-right']])
+
+        if 'header-spacing' in options:
+            opts.extend(['--header-spacing', options['header-spacing']])
+
+        if 'footer-spacing' in options:
+            opts.extend(['--footer-spacing', options['footer-spacing']])
 
     cmd = [WKHTMLTOPDF] + opts + ['-', '-']
     pdf_as_string = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE).communicate(input=html)[0]
 
-    if header:
+    if 'header' in options:
         os.remove(header_template)
-    if footer:
+    if 'footer' in options:
         os.remove(footer_template)
 
     file_object.write(pdf_as_string)
